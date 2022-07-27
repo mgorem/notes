2f5b5397b4a8e1afbe57850e3dde86eaa4d95464
mgorem <oremgitonga22@gmail.com>
2022-07-27 16:02:55 +0300

    complex state, arrays, conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { useState } from "react";
 
-// componet representing how counter will be displayed
-const Display = ({ counter }) => {
-  return <div>{counter}</div>;
+const History = ({ allClicks }) => {
+  if (allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {allClicks.join(" ")}</div>;
 };
 
 // componet representing how counter will be displayed
@@ -11,20 +13,24 @@ const Button = ({ onClick, text }) => {
 };
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter - 1);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    setLeft(left + 1);
+  };
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    setRight(right + 1);
+  };
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-
-      <Button onClick={setToZero} text="zero" />
-
-      <Button onClick={decreaseByOne} text="minus" />
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      <History allClicks={allClicks} />
     </div>
   );
 };
