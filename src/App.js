f69f2e3ff65f5d1e206e2bf4858e9567d6fb2928
mgorem <oremgitonga22@gmail.com>
2022-07-29 01:04:04 +0300

    notes app note component

--- src/App.js
+++ src/App.js
@@ -1,65 +1,16 @@
-import { useState } from "react";
+import Note from "./components/Note";
 
-const History = ({ allClicks }) => {
-  if (allClicks.length === 0) {
-    return <div>the app is used by pressing the buttons</div>;
-  }
-  return <div>button press history: {allClicks.join(" ")}</div>;
-};
-
-// componet representing how counter will be displayed
-const Button = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
-};
-
-const App = () => {
-  const [left, setLeft] = useState(0);
-  const [right, setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat("L"));
-    setLeft(left + 1);
-  };
-  const handleRightClick = () => {
-    setAll(allClicks.concat("R"));
-    setRight(right + 1);
-  };
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Button handleClick={handleLeftClick} text="left" />
-      <Button handleClick={handleRightClick} text="right" />
-      <History allClicks={allClicks} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}{" "}
+      </ul>
     </div>
   );
 };
 
 export default App;
-
-// const Hello = ({ name, age }) => {
-//   const bornYear = () => new Date().getFullYear() - age;
-
-//   return (
-//     <div>
-//       <p>
-//         Hello {name}, you are {age} years old
-//       </p>
-//       <p>So you were probably born in {bornYear()}</p>
-//     </div>
-//   );
-// };
-
-// const App = () => {
-//   const name = "Peter";
-//   const age = 10;
-
-//   return (
-//     <div>
-//       <h1>Greetings</h1>
-//       <Hello name="Maya" age={26 + 10} />
-//       <Hello name={name} age={age} />
-//     </div>
-//   );
-// };
-// export default App;
